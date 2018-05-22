a40cb4e164719b2e0111947a5224d52ba092b5c3
Plum <215747586@qq.com>
2018-05-22 23:00:08 +0800

    学习 el-tree, el-badge, el-pagination

--- src/App.vue
+++ src/App.vue
@@ -433,40 +433,109 @@
         <!--<el-button @click="addDomain">新增域名</el-button>-->
       <!--</el-form-item>-->
     <!--</el-form>-->
-    <el-table
-      :data="tableData"
-      :stripe="true"
-      border
-      height="250"
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-      <el-table-column
-        prop="tag"
-        label="标签"
-        width="100"
-        :filters="[{text: '家', value: '家'}, {text: '公司', value: '公司'}]"
-        :filter-method="filterTag"
-        filter-placement="bottom-end">
-        <template slot-scope="scope">
-          <el-tag
-            :type="scope.row.tag === '家' ? 'primary' : 'success'"
-            :disable-transitions="true">{{scope.row.tag}}</el-tag>
-        </template>
-      </el-table-column>
-    </el-table>
+    <!--<el-table-->
+      <!--:data="tableData"-->
+      <!--:stripe="true"-->
+      <!--border-->
+      <!--style="width: 100%">-->
+      <!--<el-table-column-->
+        <!--prop="date"-->
+        <!--label="日期"-->
+        <!--width="180">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--prop="name"-->
+        <!--label="姓名"-->
+        <!--width="180">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--prop="address"-->
+        <!--label="地址">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--prop="tag"-->
+        <!--label="标签"-->
+        <!--width="100"-->
+        <!--:filters="[{text: '家', value: '家'}, {text: '公司', value: '公司'}]"-->
+        <!--:filter-method="filterTag"-->
+        <!--filter-placement="bottom-end">-->
+        <!--<template slot-scope="scope">-->
+          <!--<el-tag-->
+            <!--:type="scope.row.tag === '家' ? 'primary' : 'success'"-->
+            <!--:disable-transitions="true">{{scope.row.tag}}</el-tag>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+    <!--</el-table>-->
+    <!--<el-table-->
+      <!--:data="tableData"-->
+      <!--style="width: 100%">-->
+      <!--<el-table-column-->
+        <!--label="日期"-->
+        <!--width="180">-->
+        <!--<template slot-scope="scope">-->
+          <!--<i class="el-icon-time"></i>-->
+          <!--<span style="margin-left: 10px">{{scope.row.date}}</span>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--label="姓名"-->
+        <!--width="180">-->
+        <!--<template slot-scope="scope">-->
+          <!--<el-popover trigger="hover" placement="top">-->
+            <!--<p>姓名：{{scope.row.name}}</p>-->
+            <!--<p>住址：{{scope.row.address}}</p>-->
+            <!--<div slot="reference" class="name-wrapper">-->
+              <!--<el-tag size="medium">{{scope.row.name}}</el-tag>-->
+            <!--</div>-->
+          <!--</el-popover>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column label="操作">-->
+        <!--<template slot-scope="scope">-->
+          <!--<el-button-->
+            <!--size="mini"-->
+            <!--@click="handleEdit(scope.$index, scope.row)">编辑</el-button>-->
+          <!--<el-button-->
+            <!--size="mini"-->
+            <!--type="danger"-->
+            <!--@click="handleDelete(scope.$index, scope.row)">删除</el-button>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+    <!--</el-table>-->
+    <!--<el-table-->
+      <!--:data="datas"-->
+      <!--style="width: 100%">-->
+      <!--<el-table-column type="expand">-->
+        <!--<template slot-scope="props">-->
+          <!--<el-form label-position="left" :inline="true">-->
+            <!--<el-form-item label="商品名称">-->
+              <!--<span>{{props.row.name}}</span>-->
+            <!--</el-form-item>-->
+          <!--</el-form>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--label="商品 ID"-->
+        <!--prop="id">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--label="商品名称"-->
+        <!--prop="name">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column-->
+        <!--label="描述"-->
+        <!--prop="desc">-->
+      <!--</el-table-column>-->
+    <!--</el-table>-->
+    <!--<el-tag type="success" :closable="true">标签2</el-tag>-->
+    <!--<el-tree :data="data" :props="defaultProps" :show-checkbox="true" @node-click="handleNodeClick"></el-tree>-->
+    <!--<el-pagination layout="prev, pager, ->" :total="500" background :page-size="100" :page-sizes="[100, 200]"></el-pagination>-->
+    <!--<el-badge :value="120" :max="99">-->
+      <!--<el-button size="small">评论</el-button>-->
+    <!--</el-badge>-->
+    <el-badge :is-dot="true">
+      <el-button size="small">评论</el-button>
+    </el-badge>
   </div>
 </template>
 
@@ -495,8 +564,14 @@
 
 // import ElRadioGroup from "element-ui/packages/radio/src/radio-group";
 
+// import ElTree from "element-ui/packages/tree/src/tree";
+
+// import ElPagination from "element-ui/packages/pagination/src/pagination";
+
 export default {
   components: {
+    // ElPagination
+    // ElTree
     // ElRadioGroup
     // ElTimePicker,
     // ElDatePicker
@@ -804,27 +879,68 @@ export default {
     //     email: ''
     //   }
     // }
+    // return {
+    //   tableData: [{
+    //     date: '2016-05-02',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1518 弄',
+    //     tag: '公司'
+    //   }, {
+    //     date: '2016-05-04',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1517 弄',
+    //     tag: '家'
+    //   }, {
+    //     date: '2016-05-01',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1519 弄',
+    //     tag: '公司'
+    //   }, {
+    //     date: '2016-05-03',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1516 弄',
+    //     tag: '家'
+    //   }]
+    // }
+    // return {
+    //   tableData: [{
+    //     date: '2016-05-02',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1518 弄'
+    //   }, {
+    //     date: '2016-05-04',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1517 弄'
+    //   }, {
+    //     date: '2016-05-01',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1519 弄'
+    //   }, {
+    //     date: '2016-05-03',
+    //     name: '王小虎',
+    //     address: '上海市普陀区金沙江路 1516 弄'
+    //   }]
+    // }
+    // return {
+    //   datas: [{
+    //     id: '12987122',
+    //     name: '好滋好味鸡蛋仔',
+    //     category: '江浙小吃、小吃零食',
+    //     desc: '荷兰优质淡奶，奶香浓而不腻',
+    //     address: '上海市普陀区真北路',
+    //     shop: '王小虎夫妻店',
+    //     shopId: '10333'
+    //   }]
+    // }
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄',
-        tag: '公司'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄',
-        tag: '家'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄',
-        tag: '公司'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄',
-        tag: '家'
+      data: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1'
+          }]
+        }]
       }]
     }
   },
@@ -933,11 +1049,17 @@ export default {
       return item.pinyin.indexOf(query) > -1
     }
   */
-    addDomain() {
-      this.forms.domains.push({
-        value: '',
-        key: Date.now()
-      })
+    // addDomain() {
+    //   this.forms.domains.push({
+    //     value: '',
+    //     key: Date.now()
+    //   })
+    // }，
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
     }
   }
   // mounted () {
